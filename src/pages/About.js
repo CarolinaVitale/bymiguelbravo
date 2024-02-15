701cc8f9537ffeacce9cf650cac6eed76cf9fe95
CarolinaVitale <vitale.carola@gmail.com>
2024-02-15 17:59:19 -0400

    typing letters

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,14 @@ function About() {
             <div className='container-pic'>
                 <img className='about-pic' src={miguel2} alt='miguel'></img>
             </div>
+
+            <div className="wrapper">
+                <div className="typing-demo">
+                    Hola!, soy Miguel Bravo.
+                </div>
+            </div>
             
+            <br/>
             <p>Con una formación inicial como ingeniero en 2013, Miguel descubrió su verdadera pasión en la cocina, graduándose en Le Cordon Bleu de Perú con la mención del Cordon Azul en 2015. Desde entonces, ha dejado su huella culinaria en distintas naciones, construyendo una trayectoria de casi 10 años.
             </p>
             <p>Su apasionado viaje culinario comenzó en 2015 con un emprendimiento de catering en Venezuela, donde cautivó los paladares en eventos que iban desde íntimas bodas hasta grandes eventos corporativos. El sabor de sus creaciones se extendió a España, donde trabajó de 2017 a 2022, consolidando su habilidad y amor por la cocina.
